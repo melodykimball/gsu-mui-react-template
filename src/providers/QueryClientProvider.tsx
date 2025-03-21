3359886dae2d536e2459cd5924de4ede5b978bd1
Melody Kimball <melody+github@forestkimball.com>
2025-03-21 14:16:55 -0400

    QueryClientProvider

---
+++ src/providers/QueryClientProvider.tsx
@@ -0,0 +1,17 @@
+import { type PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider as ReactQueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  },
+});
+
+export function QueryClientProvider(props: PropsWithChildren) {
+  return <ReactQueryClientProvider client={queryClient}>{props.children}</ReactQueryClientProvider>;
+}
